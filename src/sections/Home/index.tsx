89e61169ef1206f743aef6055439e41da1a7db18
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-04-11 10:38:08 -0300

    feat(section): adiciona seção Home com nome, título e botão de contato

---
+++ src/sections/Home/index.tsx
@@ -0,0 +1,17 @@
+import "./index.scss"
+
+const Home = () => {
+  return (
+    <section className="home" id="home">
+      <div className="content">
+        <h1>Gabriel</h1>
+        <h2>Desenvolvedor Full Stack</h2>
+        <a href="#contact" className="btn">
+          Me Contrate
+        </a>
+      </div>
+    </section>
+  )
+}
+
+export default Home
