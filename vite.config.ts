f545c9d28e8a27ab7fa5632e776411aaf82c287a
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-04-11 15:27:44 -0300

    adicionando a base no arquivo vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: '/Gabriel.dev/',
-=======
-  base: "/Gabriel.dev/",
->>>>>>> 039e7d6 (Instalação da dependencia gh-pages para deploy do site)
   plugins: [react()],
 })
